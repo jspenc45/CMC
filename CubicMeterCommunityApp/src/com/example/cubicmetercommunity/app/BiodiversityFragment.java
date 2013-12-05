f9db52f7204dd07841f62e4005524067659d6968
James Spencer <jspenc45@uncc.edu>
2013-12-05 17:26:44 -0500

    Biodiversity finished

--- CubicMeterCommunityApp/src/com/example/cubicmetercommunity/app/BiodiversityFragment.java
+++ CubicMeterCommunityApp/src/com/example/cubicmetercommunity/app/BiodiversityFragment.java
@@ -12,6 +12,8 @@ import com.example.cubicmetercommunity.dbutil.DBUtil;
 import com.example.cubicmetercommunityapp.R;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,10 +21,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //This fragment shows the user the Biodiversity page
 
@@ -103,7 +107,7 @@ public class BiodiversityFragment extends Fragment implements OnClickListener {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.biodiversity_activity_2, container,
+		return inflater.inflate(R.layout.biodiversity_activity, container,
 				false);
 	}
 
@@ -111,30 +115,56 @@ public class BiodiversityFragment extends Fragment implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.act_finish:
-			TableLayout table = (TableLayout) getActivity().findViewById(
-					R.id.TableLayout1);
-			Map<String, Object> fields = new HashMap<String, Object>();
-			List<String> fieldList = Naturalist.returnFieldList();
-			Iterator<String> it = fieldList.iterator();
-
-			for (int i = 0; i < table.getChildCount(); i++) {
-				TableRow row = (TableRow) table.getChildAt(i);
-
-				for (int j = 0; j < row.getChildCount(); j++) {
-					View view = row.getChildAt(j);
-
-					int count = Integer
-							.parseInt(((TextView) view
-									.findViewById(R.id.textView1)).getText()
-									.toString());
-					fields.put(it.next(), count);
-				}
+			String numOrg = ((EditText) getActivity().findViewById(
+					R.id.EditText01)).getText().toString();
+			String numTypes = ((EditText) getActivity().findViewById(
+					R.id.EditText01)).getText().toString();
+			if (numOrg.equals("") || numTypes.equals("")) {
+				showToast();
+			} else {
+				new AlertDialog.Builder(getActivity())
+						.setTitle("Finish")
+						.setMessage("Are you done?")
+						.setIcon(android.R.drawable.ic_dialog_alert)
+						.setPositiveButton("Yes",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int whichButton) {
+										submitResults();
+									}
+								}).setNegativeButton("No", null)
+						.show();
 			}
-			DBUtil db = new DBUtil();
-			TableIDs ids = ((IndividualActivity)getActivity()).getTables();
-			db.update("Naturalist__c", ids.getNaturalistID(), fields);
-
 		}
+	}
 
+	public void submitResults() {
+		TableLayout table = (TableLayout) getActivity().findViewById(
+				R.id.TableLayout1);
+		Map<String, Object> fields = new HashMap<String, Object>();
+		List<String> fieldList = Naturalist.returnFieldList();
+		Iterator<String> it = fieldList.iterator();
+
+		for (int i = 0; i < table.getChildCount(); i++) {
+			TableRow row = (TableRow) table.getChildAt(i);
+
+			for (int j = 0; j < row.getChildCount(); j++) {
+				View view = row.getChildAt(j);
+
+				int count = Integer.parseInt(((TextView) view
+						.findViewById(R.id.textView1)).getText().toString());
+				fields.put(it.next(), count);
+			}
+		}
+		DBUtil db = new DBUtil();
+		TableIDs ids = ((IndividualActivity) getActivity()).getTables();
+		db.update("Naturalist__c", ids.getNaturalistID(), fields);
+
+		getActivity().finish();
+	}
+
+	public void showToast() {
+		Toast.makeText(getActivity(), "One of your boxes is blank!",
+				Toast.LENGTH_LONG).show();
 	}
 }
